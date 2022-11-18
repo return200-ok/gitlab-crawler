ddddeb088ea875f024d7021c0f8118913942d784
caolv <caolv@biplus.com.vn>
2022-11-18 11:50:24 +0700

    .

--- gitlab_collector.py
+++ gitlab_collector.py
@@ -7,6 +7,7 @@ from time import time
 
 import rfc3339
 from dotenv import load_dotenv
+from influx_client import InfluxClient, InfluxPoint
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import ASYNCHRONOUS
 
@@ -26,12 +27,25 @@ def get_date_string(date_object):
 
 duration_time = datetime.now() - timedelta(before_day)
 log_file = get_date_string(datetime.now())+'_gitlab_collecter.log'
-logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-
+# instantiate a logger object 
+logging.basicConfig(
+  format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+  datefmt='%Y-%m-%d:%H:%M:%S',
+  level=logging.DEBUG,
+  filename='logs/'+log_file
+)
+logger = logging.getLogger(__name__)
 
 # List projects
+'''
+    UserWarning: Calling a `list()` method without specifying `get_all=True` or `iterator=True` 
+    will return a maximum of 20 items. Your query returned 20 of many items. 
+    See https://python-gitlab.readthedocs.io/en/v3.11.0/api-usage.html#pagination for more details. 
+    If this was done intentionally, 
+    then this warning can be supressed by adding the argument `get_all=False` to the `list()` call.
+'''
 def get_projects():
-    projects = gl.projects.list()
+    projects = gl.projects.list(get_all=True)
     return projects
 
 #Get the list of branches for a repository
@@ -89,9 +103,8 @@ class Mrs:
 def gen_datapoint(kpi_type, kpi_data, i):
     if kpi_type == "mrs":
         data = Mrs(kpi_data[i].id, kpi_data[i].project_id, kpi_data[i].title, kpi_data[i].state, kpi_data[i].created_at, kpi_data[i].updated_at, kpi_data[i].target_branch, kpi_data[i].source_branch)
-        data_point = [{
-            "measurement": project.id,
-            "tags": {
+        measurement = project.id
+        tags = {
             "project_name": project.name,
             "mrs_id": data.id,
             "mrs_title": data.title,
@@ -100,47 +113,45 @@ def gen_datapoint(kpi_type, kpi_data, i):
             "updated_at": data.updated_at,
             "target_branch": data.target_branch,
             "source_branch": data.source_branch,
-            },
-            "time": int(time()) * 1000000000,
-            "fields": {
+            }
+        timestamp = int(time())
+        fields = {
             "mrs_id": data.id
             }
-        }]
+        data_point = InfluxPoint(measurement, tags, fields, timestamp)._point
         return data_point
     elif kpi_type == "issue":
         data = Issues(kpi_data[i].id, kpi_data[i].project_id, kpi_data[i].title, kpi_data[i].state, kpi_data[i].created_at, kpi_data[i].updated_at)
-        data_point = [{
-            "measurement": data.project_id,
-            "tags": {
+        measurement = project.id
+        tags = {
             "project_name": project.name,
             "issue_id": data.id,
             "issue_title": data.title,
             "issue_state": data.state,
             "created_at": data.created_at,
             "updated_at": data.updated_at,
-            },
-            "time": int(time()) * 1000000000,
-            "fields": {
+            }
+        timestamp = int(time())
+        fields = {
             "issue_id": data.id
             }
-        }]
+        data_point = InfluxPoint(measurement, tags, fields, timestamp)._point
         return data_point
     elif kpi_type == "commit":
         data = Commits(kpi_data[i].id, kpi_data[i].short_id, kpi_data[i].title, kpi_data[i].created_at, kpi_data[i].author_email)
-        data_point = [{
-            "measurement": project.id,
-            "tags": {
+        measurement = project.id
+        tags = {
             "project_name": project.name,
             "commit_id": data.id,
             "commit_title": data.title,
             "created_at": data.created_at,
             "author_email": data.author_email,
-            },
-            "time": int(time()) * 1000000000,
-            "fields": {
+            }
+        timestamp = int(time())
+        fields = {
             "commit_id": data.short_id
             }
-        }]
+        data_point = InfluxPoint(measurement, tags, fields, timestamp)._point
         return data_point
     else:
         logging.warning("kpi_type is not matching!")
@@ -158,6 +169,20 @@ def push_data(kpi_type, kpi_data):
                     logging.error("Problem inserting points for current batch")
                     raise e
 
+def push_data_to_influx(kpi, data):
+    client = InfluxClient(influx_server, influx_token, org_name, bucket_name)
+    client.check_connection()
+    client.check_write()
+    if len(data) > 0:
+        for i in range(0, len(data)):
+            data_point = gen_datapoint(kpi, data, i)
+            try:
+                client.write_data(data_point)
+                logging.info("Wrote "+str(data_point)+" to bucket "+bucket_name)
+            except Exception as e:
+                logging.error("Problem inserting points for current batch")
+                raise e
+
 if __name__ == '__main__':
     projects = get_projects()
     kpis = ["mrs","issue","commit"]
@@ -165,13 +190,13 @@ if __name__ == '__main__':
         if kpi == "mrs":
             for project in projects:
                 mrs = get_mrs(project)
-                push_data(kpi, mrs)
+                push_data_to_influx(kpi, mrs)
         elif kpi == "issue":
             for project in projects:
                 issue = get_issues(project)
-                push_data(kpi, issue)
+                push_data_to_influx(kpi, issue)
         elif kpi == "commit":
             for project in projects:
                 commit = get_commits(project)
-                push_data(kpi, commit)
+                push_data_to_influx(kpi, commit)
 
---
+++ influx_client.py
@@ -0,0 +1,180 @@
+import datetime
+import logging
+import os
+from datetime import datetime, timedelta
+from time import time
+
+from influxdb_client import Dialect, InfluxDBClient, Point
+from influxdb_client.client.write_api import ASYNCHRONOUS, SYNCHRONOUS
+from influxdb_client.rest import ApiException
+
+logger = logging.getLogger(__name__)
+
+class InfluxPoint:
+    
+    """Initialize defaults."""
+    def __init__(self, measurement, tags, fields, timestamp):
+        self.measurement = measurement
+        self.tags = tags
+        self.fields = fields
+        self.timestamp = timestamp
+        
+        '''
+        Example:
+            tags = {"stock": "MSFT", "stock_id": "abc789"}
+            fields = {"Open": 68, "High": 69.38, "Low": 60.13,}
+            timestamp = int(time())
+            data_point = InfluxPoint(measurement, tags, fields, timestamp)
+        '''
+        self._point = [{
+            "measurement": measurement,
+            "tags": tags,
+            "time": timestamp,
+            "fields": fields
+        }]
+
+class InfluxClient:
+    
+    '''
+    Initialize defaults.
+    Usage: client = InfluxClient(influx_server, influx_token, org_name, bucket_name)
+    Example:
+        client = InfluxClient("http://localhost:8086", "KlXfBqa0uSGs0icfE", "org", "bucket")
+        client.write_data(data)
+    '''
+    def __init__(self, url, token, org, bucket): 
+        self._org = org
+        self._bucket = bucket
+        self._client = InfluxDBClient(url=url, token=token, org=org)
+    
+    '''
+    Check connection to InfluxDb
+    Example:
+        client = InfluxClient(influx_server, influx_token, org_name, bucket_name)
+        client.check_connection()
+    '''
+    def check_connection(self):
+        """Check that the InfluxDB is running."""
+        print("> Checking connection ...", end=" ")
+        self._client.api_client.call_api('/ping', 'GET')
+        print("ok")
+        
+    '''
+    Check query to InfluxDB
+    Example:
+        client = InfluxClient(influx_server, influx_token, org_name, bucket_name)
+        client.check_query()
+    '''
+    def check_query(self, query):
+        """Check that the credentials has permission to query from the Bucket"""
+        print("> Checking credentials for query ...", end=" ")
+        query_api = self._client.query_api()
+        result = query_api.query(org=self._org, query=query)
+        try:
+            result
+        except ApiException as e:
+            # missing credentials
+            if e.status == 404:
+                raise Exception(f"The specified token doesn't have sufficient credentials to read from '{self._bucket}' "
+                                f"or specified bucket doesn't exists.") from e
+            raise
+        print("ok")
+
+    def check_write(self):
+        """Check that the credentials has permission to write into the Bucket"""
+        print("> Checking credentials for write ...", end=" ")
+        try:
+            self._client.write_api(write_options=SYNCHRONOUS).write(self._bucket, self._org, b"")
+        except ApiException as e:
+            # bucket does not exist
+            if e.status == 404:
+                logging.exception("The specified bucket does not exist.")
+                raise Exception(f"The specified bucket does not exist.") from e
+            # insufficient permissions
+            if e.status == 403:
+                raise Exception(f"The specified token does not have sufficient credentials to write to '{self._bucket}'.") from e
+            # 400 (BadRequest) caused by empty LineProtocol
+            if e.status != 400:
+                raise
+        print("ok")
+
+    '''
+    Method called write_api which is used to write data into your database
+    Example:
+        data_point=["MSFT,stock=MSFT Open=62.79,High=63.84,Low=62.13"]
+        client.write_data(data_point)
+    '''
+    def write_data(self, data, write_option=SYNCHRONOUS):
+        write_api = self._client.write_api(write_option)
+        write_api.write(self._bucket, self._org, data, write_precision='s')
+    
+    '''
+    Method called query_data used to read data 
+    Example:
+        query = 'from(bucket: "test") |> range(start: -1h)'
+        client.query_data(query)
+    '''
+    def query_data(self, query):
+        query_api = self._client.query_api()
+        result = query_api.query(org=self._org, query=query)
+        # print(result.to_json(indent=5))
+        results = []
+        for table in result:
+            for record in table.records:
+                results.append((record.get_measurement(), record.get_value(), record.get_field()))
+        return results 
+    
+    '''
+    Query data 
+    Serialize to JSON
+    ''' 
+    def query_response_to_json(self, query):
+        query_api = self._client.query_api()
+        result = query_api.query(org=self._org, query=query)
+        results = []
+        for table in result:
+            for record in table.records:
+                results.append((record.values))
+        return results 
+    
+    '''
+    Method called delete_data used to delete data 
+    Example:
+        start_time = "2022-11-10T08:26:51.098Z" # type string: timestamp rfc3339
+        stop_time = "2022-11-17T00:00:00.098Z"
+        client.delete_data(start_time, stop_time, measurement)
+    '''
+    def delete_data(self, start, stop, measurement):
+        delete_api = self._client.delete_api()
+        self.start = start
+        self.stop = stop
+        delete_api.delete(start, stop, f'_measurement="{measurement}"', bucket=self._bucket, org=self._org)
+
+
+ 
+# client = InfluxClient(influx_server, influx_token, org_name, bucket_name)
+# query = 'from(bucket: "test") |> range(start: -7d)'
+# start_time = "2022-11-10T08:26:51.098Z"
+# stop_time = "2022-11-18T00:00:00.098Z"
+# tags = {"stock": "MSFT", "stock_id": "abc789"}
+# fields = {"Open": 68, "High": 69.38, "Low": 60.13,}
+# timestamp = int(time())
+# data_point = InfluxPoint(measurement, tags, fields, timestamp)
+# r = client.query_response_to_json(query)
+
+# print(data_point._point)
+# client.query_data(query)
+# print(type(r))
+# client.check_connection()
+# client.check_query(query)
+# client.check_write()
+# client.delete_data(start_time, stop_time, measurement)
+# client.write_data(data_point._point)
+
+
+
+
+
+
+
+    
